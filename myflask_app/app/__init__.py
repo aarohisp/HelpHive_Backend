847c2697a6acce03277b06cf202481d3b5558cdd
aarohisp <112809842+aarohisp@users.noreply.github.com>
2023-10-13 16:54:12 +0530

    "current_app" to avoid working out of app context

--- myflask_app/app/__init__.py
+++ myflask_app/app/__init__.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-
+    
     # Load configuration settings from config.py
     config_file_path = os.path.abspath('./config.py')
     app.config.from_pyfile(config_file_path)
@@ -16,5 +16,6 @@ def create_app():
 
     # Import your routes
     from apis import routes
+    routes.init_routes(app)
 
     return app
