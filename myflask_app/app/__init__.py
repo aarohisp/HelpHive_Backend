c95766c9c05bd340a5df8ba4af53fd880901a336
aarohisp <112809842+aarohisp@users.noreply.github.com>
2023-10-13 16:26:05 +0530

    added requirements file

--- myflask_app/app/__init__.py
+++ myflask_app/app/__init__.py
@@ -8,13 +8,13 @@ def create_app():
     app = Flask(__name__)
 
     # Load configuration settings from config.py
-    config_file_path = os.path.abspath('D:\Documents\GitHub\HelpHive_Backend\myflask_app\config.py')
+    config_file_path = os.path.abspath('./config.py')
     app.config.from_pyfile(config_file_path)
 
     # Initialize the database
     db.init_app(app)
 
     # Import your routes
-    from app import routes
+    from apis import routes
 
     return app
